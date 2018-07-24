8638828ebc6b4fd56e3f03314cc73888480188a6
DanigCuevas <41652996+DanigCuevas@users.noreply.github.com>
2018-07-24 15:46:57 -0700

    Create website.js

---
+++ website.js
@@ -0,0 +1,18 @@
+var pictures=1;
+showDivs(Pictures);
+
+function plusDivs(n){
+   showDivs(Pictures+=n);
+}
+
+function showDivs(n){
+  var i;
+  var x =document.getElementsByClassName("pictureSlide");
+  if ( n > x.length) {pictures= 1}
+  if (n < 1) {pictures= x.length};
+  for (i =0;i < x.length; i++){
+    x[i].style.display ="none";
+  }
+  x[pictures-1].style.display ="block" ;
+
+}
