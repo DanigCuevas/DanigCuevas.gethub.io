3525a82fb063faa8458c692f4bd79bcc6c3227ea
DanigCuevas <41652996+DanigCuevas@users.noreply.github.com>
2018-07-27 10:37:29 -0700

    Update website.js

--- website.js
+++ website.js
@@ -1,3 +1,24 @@
+Var slideindex= 1;
+showSlides(slideindex);
+
+function plusSlides(x){
+   showSlides(slideindex +=x);
+}
+function showSlides(x){
+   var i;
+   var slides= document.getElementByClassname('slide');
+   if (x > slides.length){
+      slideindex= 1
+}
+}
+   if (x<1){
+      slideindex=slides.length;
+   }
+   for (i=0; i<slides.length; i++){
+      slides[i].style.display ="none";
+   }
+   slides[slideindex-1].style.display="block";
+ //navigation bar stuff  
 function makeGray(x){
 x.style.backgroundColor="#999999";
 }
